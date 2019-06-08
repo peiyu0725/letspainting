1c6ce341255e8b2559daea695d1ca47d8b587b7b
peiyu0725 <b20321115@gmail.com>
2019-06-08 16:17:52 +0800

    test

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/linewebhook', linebotParser);
 
 // var upload = multer({ dest: 'fileupload/'});
 
-app.post('/fileupload', function(req, res) {
+app.post('/fileupload', upload.single('file'), function(req, res) {
 	console.log(req.file);
 	var file = 'uploads' + '/' + req.file.originalname;
   fs.rename(req.file.path, file, function(err) {
