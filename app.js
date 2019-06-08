6ec0231fae4ada9dfc496df793a1de200c816b9d
peiyu0725 <b20321115@gmail.com>
2019-06-08 20:11:49 +0800

    post file

--- app.js
+++ app.js
@@ -34,20 +34,28 @@ var upload = multer({ dest: 'upload/'});
 var type = upload.single('file');
 
 app.post('/fileupload', type, function (req,res) {
+	console.log(req.file);
 
   /** When using the "single"
       data come in "req.file" regardless of the attribute "name". **/
   var tmp_path = req.file.path;
-
   /** The original name of the uploaded file
       stored in the variable "originalname". **/
-  var target_path = 'uploads/' + req.file.originalname;
+  var target_path = 'upload/' + req.file.originalname;
 
   /** A better way to copy the uploaded file. **/
   var src = fs.createReadStream(tmp_path);
   var dest = fs.createWriteStream(target_path);
   src.pipe(dest);
-  src.on('end', function() { res.render('complete'); });
+  src.on('end', function() {
+		var response = {
+			status : true,
+			image: req.file.originalname
+		}
+		res.end(JSON.stringify(response));
+		//
+		// res.render('complete');
+});
   src.on('error', function(err) { res.render('error'); });
 });
 // app.post('/fileupload', multer(multerConfig).single('photo'), function(req, res) {
